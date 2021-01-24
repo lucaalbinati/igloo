465ed7aaa97935b2e77922a3162fd51a6ef809eb
Luca Albinati <luca.albinati@gmail.com>
2021-01-24 09:58:33 +0100

    after structural changes -> switch focus from project to addon

--- run_script.py
+++
@@ -1,9 +0,0 @@
-import bpy
-import os
-import sys
-
-# add python 'igloo' src folder to path
-sys.path.append('/Users/lucaalbinati/Documents/MyDocuments/Programmation/blender/Igloo/igloo')
-
-filename = '/Users/lucaalbinati/Documents/MyDocuments/Programmation/blender/Igloo/igloo/main.py'
-exec(compile(open(filename).read(), filename, 'exec'))
