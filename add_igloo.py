902d1fb749e27f97262a56172ed4819322988dae
Luca Albinati <luca.albinati@gmail.com>
2021-03-01 19:57:55 +0100

    fix negative face bug

--- add_igloo.py
+++ add_igloo.py
@@ -4,12 +4,15 @@ import numpy as np
 
 import bpy
 import bmesh
-from bpy.props import IntProperty, FloatProperty
+from bpy.props import BoolProperty, IntProperty, FloatProperty
 
 from .utils import object_mode, edit_mode, select_objs, select_obj, delete_objs, delete_obj
 
 IGLOO_OBJ_NAME = 'igloo'
 IGLOO_TOP_OBJ_NAME = 'igloo_top'
+
+HIGH_PRECISION = 128
+LOW_PRECISION = 32
 
 class Igloo(bpy.types.Operator):
 
@@ -17,12 +20,10 @@ class Igloo(bpy.types.Operator):
 	bl_label = "Igloo"
 	bl_options = {'REGISTER', 'UNDO'}
 	
-	precision : IntProperty(
-		name = "Precision",
-		description = "The number of vertices of the igloo's sphere",
-		default = 32,
-		min = 16,
-		max = 256
+	high_precision : BoolProperty(
+		name = "High Precision",
+		description = "Whether to use high or low precision",
+		default = False
 	)
 
 	radius : FloatProperty(
@@ -66,10 +67,6 @@ class Igloo(bpy.types.Operator):
 	)
 
 	def execute(self, context):
-		if self.precision % 2 == 1:
-			self.report({"ERROR"}, "The number of precision must be a multiple of 2")
-			return {"CANCELLED"}
-
 		if 90 % self.nb_bricks_vertical != 0:
 			self.report({"ERROR"}, "The number of vertical bricks must be a multiple of 90 (i.e., 2, 3, 5, 6, 9, 15, 18, etc…)")
 			return {"CANCELLED"}
@@ -87,20 +84,22 @@ class Igloo(bpy.types.Operator):
 		igloo_obj = self.__create_igloo_base()
 		print("Created igloo demi-sphere.")
 
-		#return None, None
-		# Cut off and separate igloo top
+		# Divide the igloo vertically
 		vertical_angle = int(90 / self.nb_bricks_vertical)
-		igloo_obj, igloo_top_obj, igloo_top_radius = self.__separate_igloo_top(igloo_obj, vertical_angle)
-		print("Separated igloo top from igloo.")
-
-		# Divide the rest, vertically
 		vertical_angles = []
-		curr = 2 * vertical_angle
+		curr = vertical_angle
 		while curr < 90:
 			vertical_angles.append(curr)
 			curr += vertical_angle
-		self.__create_cones_and_cut_igloo(igloo_obj, vertical_angles)
+		igloo_top_radius = self.__create_cones_and_cut_igloo(igloo_obj, vertical_angles)
 		print("Cut igloo, vertically.")
+
+		# Separate the top from the rest
+		igloo_obj, igloo_top_obj = self.__separate_igloo_top(igloo_obj)
+		print("Separate igloo top from the rest.")
+
+		# Fix bottom face bug
+		self.__fix_negative_bug(igloo_obj)
 
 		# Divide the rest, radially
 		radial_angle = int(360 / self.nb_bricks_radial)
@@ -118,10 +117,10 @@ class Igloo(bpy.types.Operator):
 		return igloo_obj, igloo_top_obj
 
 	def __create_igloo_base(self):
-		#sphere_obj.name = IGLOO_OBJ_NAME
 		def create_demi_sphere(radius):
 			# Create sphere object
-			bpy.ops.mesh.primitive_uv_sphere_add(segments=self.precision, ring_count=self.precision, radius=radius, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+			precision = HIGH_PRECISION if self.high_precision else LOW_PRECISION
+			bpy.ops.mesh.primitive_uv_sphere_add(segments=precision, ring_count=precision, radius=radius, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 			sphere_obj = bpy.context.active_object
 
 			# Remove bottom half
@@ -175,14 +174,17 @@ class Igloo(bpy.types.Operator):
 		object_mode()
 
 		# Create all cones
+		vertical_angles = sorted(vertical_angles, reverse=True) # such that the variable 'radius' holds the radius of the top part (i.e. the last part)
 		cone_objs = []
 		for vertical_angle in vertical_angles:
 			# Compute cone dimensions based on 'vertical_angle'
 			depth = self.radius
 			radius = depth * math.tan(math.radians(vertical_angle))
+			print("{}: {}".format(vertical_angle, radius))
 
 			# Create cone
-			bpy.ops.mesh.primitive_cone_add(radius1=radius, depth=depth, vertices=self.precision, end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+			precision = HIGH_PRECISION if self.high_precision else LOW_PRECISION
+			bpy.ops.mesh.primitive_cone_add(radius1=radius, depth=depth, vertices=precision, end_fill_type='NOTHING', enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 			cone_objs.append(bpy.context.active_object)
 			bpy.ops.transform.rotate(value=math.radians(180), orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
 			bpy.ops.transform.translate(value=(0, 0, depth/2), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
@@ -201,6 +203,7 @@ class Igloo(bpy.types.Operator):
 		select_obj(igloo_obj)
 		bpy.ops.object.modifier_add(type='BOOLEAN')
 		bpy.context.object.modifiers["Boolean"].object = difference_obj
+		bpy.context.object.modifiers["Boolean"].solver = 'FAST'
 		bpy.ops.object.modifier_apply(modifier="Boolean")
 
 		# Delete the difference object
@@ -208,30 +211,37 @@ class Igloo(bpy.types.Operator):
 
 		return radius
 
-	def __create_cone_and_cut_igloo(self, igloo_obj, vertical_angle):
-		return self.__create_cones_and_cut_igloo(igloo_obj, [vertical_angle])
-
-	def __separate_igloo_top(self, igloo_obj, vertical_angle):
-		radius = self.__create_cone_and_cut_igloo(igloo_obj, vertical_angle)
-
-		# Separate top from igloo
+	def __separate_igloo_top(self, igloo_obj):
 		select_obj(igloo_obj)
 		edit_mode()
 		bpy.ops.mesh.separate(type="LOOSE")
 		object_mode()
-		
-		# Find which object is the top, using their z-coordinate (after applying their origin to their respective geometry)
-		objs = [igloo_obj, bpy.data.objects[igloo_obj.name + ".001"]]
+
+		objs = [obj for obj in bpy.data.objects if obj.name.startswith("igloo")]
 		for obj in objs:
-			select_obj(obj)
 			bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
+		igloo_top_obj = sorted(objs, key=lambda o: o.location[2], reverse=True)[0]
+		igloo_top_obj.name = IGLOO_TOP_OBJ_NAME
 
-		objs = sorted(objs, key=lambda obj: obj.location[2])
+		select_objs([obj for obj in objs if obj != igloo_top_obj])
+		bpy.ops.object.join()
+		igloo_obj = bpy.context.active_object
+		igloo_obj.name = IGLOO_OBJ_NAME
 
-		objs[0].name = IGLOO_OBJ_NAME
-		objs[1].name = IGLOO_TOP_OBJ_NAME
-		
-		return objs[0], objs[1], radius
+		bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
+
+		return igloo_obj, igloo_top_obj
+
+	def __fix_negative_bug(self, igloo_obj):
+		edit_mode()
+		bpy.ops.mesh.select_all(action='DESELECT')
+		bm = bmesh.from_edit_mesh(igloo_obj.data)
+		for face in bm.faces:
+			for vert in face.verts:
+				if vert.co[2] < 0 and not np.isclose(vert.co[2], 0, rtol=0.0001, atol=0.0001):
+					face.select = True
+		bpy.ops.mesh.delete(type='FACE')
+		object_mode()
 
 	def __create_planes_and_cut_igloo(self, igloo_obj, igloo_top_radius, z_rotations):
 		object_mode()
@@ -261,6 +271,7 @@ class Igloo(bpy.types.Operator):
 		select_obj(igloo_obj)
 		bpy.ops.object.modifier_add(type='BOOLEAN')
 		bpy.context.object.modifiers["Boolean"].object = difference_obj
+		bpy.context.object.modifiers["Boolean"].solver = 'FAST'
 		bpy.ops.object.modifier_apply(modifier="Boolean")
 
 		# Remove the difference object
